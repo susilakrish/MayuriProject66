bf58ff5562d764ec7dda19b3fe5fcbc63dde8963
susilakrish <susilakrish2096@gmail.com>
2024-07-21 18:59:59 +0530

    First time push

---
+++ src/test/java/org/git/ClassA.java
@@ -0,0 +1,18 @@
+package org.git;
+
+public class ClassA {
+	
+		public void method1() {
+			System.out.println("Java");
+		}
+		
+		public void method2() {
+			System.out.println("Selenium");
+		}
+		
+		public void method3() {
+			System.out.println("Cucumber");
+		}
+	}
+
+
