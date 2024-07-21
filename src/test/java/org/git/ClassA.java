24a425290e2605b5c10822d4fa30d423df9c334d
susilakrish <susilakrish2096@gmail.com>
2024-07-21 20:37:36 +0530

    Added 3 methods from Tester62

--- src/test/java/org/git/ClassA.java
+++ src/test/java/org/git/ClassA.java
@@ -13,6 +13,17 @@ public class ClassA {
 		public void method3() {
 			System.out.println("Cucumber");
 		}
+		public void method4() {
+			System.out.println("Git");
+		}
+		
+		public void method5() {
+			System.out.println("Jenkins");
+		}
+		
+		public void method6() {
+			System.out.println("TestNg");
+		}
 	}
 
 
